4db1da27ebcc587ac3d19aa73eeb67ad6bac55de
chenhao <1223595056@qq.com>
2020-01-05 17:25:35 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'amfe-flexible'
 Vue.config.productionTip = false
 
 new Vue({
